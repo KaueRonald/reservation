a85c71cb5c216d7e9d00affc0ebbba33a51d772e
KaueRonald <Kaueronald21.kr@gmail.com>
2024-05-03 17:53:55 -0300

    feat: login service

---
+++ reservation-backend/src/services/UserService.ts
@@ -0,0 +1,32 @@
+import HttpStatusCode from '../utils/enum/HttpStatusCode';
+import { UserRepository } from '../database/User.repository';
+import { generateToken } from '../utils/GenerateToken';
+import bcrypt from 'bcrypt';
+
+export default class UserServices {
+    public repository: UserRepository;
+
+    constructor() {
+        this.repository = new UserRepository();
+    }
+
+    public verifyUserCredentials = async (email: string, password: string) => {
+        const user = await this.repository.getUserByEmail(email);
+        if (!user) {
+            throw {
+                status: HttpStatusCode.NOT_FOUND,
+                message: { error: 'User NOT FOUND' },
+            };
+        }
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            throw {
+                status: HttpStatusCode.BAD_REQUEST,
+                message: { error: 'Invalid password' },
+            };
+        }
+        const token = generateToken(user.id, user.role);
+        
+        return { token };
+    }
+}
