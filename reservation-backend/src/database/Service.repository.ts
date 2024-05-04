008026095169049fb825e3f303bdc6e913359509
KaueRonald <Kaueronald21.kr@gmail.com>
2024-05-04 16:00:02 -0300

    feat: repository for Read, Update and delete for the service

--- reservation-backend/src/database/Service.repository.ts
+++ reservation-backend/src/database/Service.repository.ts
@@ -4,13 +4,53 @@ import { Service } from '@prisma/client'
 export class ServiceRepository {
 
     public createService = async (data: Service, id: string) => {
-        const service = await prisma.service.create({ data: {
-            name: data.name,
-            description: data.description,
-            price: data.price,
-            id_user: id,
-            type: data.type
-        }  })
+        const service = await prisma.service.create({
+            data: {
+                name: data.name,
+                description: data.description,
+                price: data.price,
+                id_user: id,
+                type: data.type
+            }
+        })
+        return service;
+    }
+
+    public getAllServices = async () => {
+        const services = await prisma.service.findMany();
+        return services;
+    }
+
+    public getServiceById = async (id: string) => {
+        const service = await prisma.service.findUnique({
+            where: {
+                id: id
+            }
+        })
+        return service;
+    }
+
+    public updateService = async (id: string, data: Service) => {
+        const service = await prisma.service.update({
+            where: {
+                id: id
+            },
+            data: {
+                name: data.name,
+                description: data.description,
+                price: data.price,
+                type: data.type
+            }
+        })
+        return service;
+    }
+
+    public deleteService = async (id: string) => {
+        const service = await prisma.service.delete({
+            where: {
+                id: id
+            }
+        })
         return service;
     }
 }
