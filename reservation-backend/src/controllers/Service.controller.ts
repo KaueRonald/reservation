b8e73345e1440e6a04121f202e661cb6b9c1061f
KaueRonald <Kaueronald21.kr@gmail.com>
2024-05-04 16:00:59 -0300

    feat: add controllers for Read, Update and delete Services

--- reservation-backend/src/controllers/Service.controller.ts
+++ reservation-backend/src/controllers/Service.controller.ts
@@ -24,13 +24,56 @@ export class ServiceController {
         const [bearer, token] = prefixtoken.split(' ');
         try {
             VerifyRole(request, response, async () => {
-                const serviceData = request.body; 
+                const serviceData = request.body;
                 const decoded = jwt.verify(token, `${process.env.TOKEN_KEY}`) as TokenPayload;
                 const service = await this.serviceRepository.createService(serviceData, decoded.userId.toString());
                 response.status(HttpStatusCode.OK).json(service);
             });
         } catch (error) {
             response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(error);
+        }
+    }
+
+    public getAllServices = async (request: Request, response: Response) => {
+        try {
+            const service = await this.serviceRepository.getAllServices();
+            response.status(HttpStatusCode.OK).send(service);
+        } catch (error) {
+            response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(error);
+        }
+    }
+
+    public getServicesById = async (request: Request, response: Response) => {
+        try {
+            const serviceid = await request.params.id
+            if (!serviceid) {
+                return response.status(HttpStatusCode.BAD_REQUEST).send("O id não foi fornecido.");
+            }
+            const service = await this.serviceRepository.getServiceById(serviceid);
+            if (!service) {
+                return response.status(HttpStatusCode.NOT_FOUND).send("Serviço não encontrado.");
+            }
+            response.status(HttpStatusCode.OK).send(service);
+        } catch (error) {
+            response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(error);
+        }
+    }
+
+    public updateServiceById = async (request: Request, response: Response) => {
+        try {
+            const service = await this.serviceRepository.updateService(request.params.id, request.body);
+            response.status(HttpStatusCode.OK).send(service)
+        } catch (error) {
+            response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send("erro a atualizar o serviço.")
+        }
+    }
+
+    public deleteServiceById = async (request: Request, response: Response) => {
+        try {
+            const service = await this.serviceRepository.deleteService(request.params.id);
+            response.status(HttpStatusCode.OK).send("serviço deletado com sucesso!");
+        } catch (error) {
+            response.status(HttpStatusCode.BAD_REQUEST).send("Não foi possível excluir o serviço")
         }
     }
 }
