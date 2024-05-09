fd3d7c0d4929d38102a62fa61f7e6cc79483a2f7
KaueRonald <Kaueronald21.kr@gmail.com>
2024-05-09 15:06:44 -0300

    refactor: adding services from de routes controllers

--- reservation-backend/src/controllers/User.controller.ts
+++ reservation-backend/src/controllers/User.controller.ts
@@ -1,25 +1,17 @@
-import { UserRepository } from "../database/User.repository";
 import { Request, Response } from "express";
 import HttpStatusCode from "../utils/enum/HttpStatusCode";
-import bcrypt from "bcrypt";
-import { userValidation } from "../validations/User.validation";
-import UserServices from "../services/UserService";
+import { UserServices } from "../services/UserService";
 
 export class UserController {
-    private userRepository: UserRepository;
     private userService: UserServices
 
     constructor() {
-        this.userRepository = new UserRepository();
         this.userService = new UserServices();
     }
 
     public createUsers = async (request: Request, response: Response) => {
         try {
-            await userValidation.validate(request.body);
-            const hashPassword = await bcrypt.hash(request.body.password, 10);
-            request.body.password = hashPassword;
-            const user = await this.userRepository.createUser(request.body);
+            const user = await this.userService.create(request.body);
             response.status(HttpStatusCode.CREATED).send(user);
         } catch (error) {
             response.status(HttpStatusCode.BAD_REQUEST).send(error);
@@ -27,65 +19,34 @@ export class UserController {
     }
 
     public getAllUsers = async (request: Request, response: Response) => {
-        try {
-            const users = await this.userRepository.getUsers();
-            response.status(HttpStatusCode.OK).send(users);
-        } catch (error) {
-            response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(error);
-        }
+        const users = await this.userService.getUsers();
+        response.status(HttpStatusCode.OK).send(users);
     }
 
     public getUsersById = async (request: Request, response: Response) => {
         try {
-            const ServiceId = await request.params.id
-            if (!ServiceId) {
-                return response.status(HttpStatusCode.BAD_REQUEST).send("O id não foi fornecido.");
-            }
-            const user = await this.userRepository.getUserById(ServiceId);
-            if (!user) {
-                return response.status(HttpStatusCode.NOT_FOUND).send("Usuário não encontrado.");
-            }
+            const user = await this.userService.getUserById(request.params.id);
             response.status(HttpStatusCode.OK).send(user);
         } catch (error) {
-            response.status(HttpStatusCode.NOT_FOUND).send(error);
+            response.status(HttpStatusCode.NOT_FOUND).send(error instanceof Error ? error.message : "Erro na solicitação.");
         }
     }
 
     public updateUserById = async (request: Request, response: Response) => {
         try {
-            if (request.body.role === "CLIENT") {
-                const user = await this.userRepository.getUserById(request.params.id);
-                if (user && user.services.length > 0) {
-                    return response.status(HttpStatusCode.FORBIDDEN).send('Usuário com serviços não pode ser definido como CLIENT.');
-                }
-            }
-            await userValidation.validate(request.body);
-            const hashPassword = await bcrypt.hash(request.body.password, 10);
-            request.body.password = hashPassword;
-            const user = await this.userRepository.updateUser(request.params.id, request.body);
+            const user = await this.userService.updateUser(request.params.id, request.body);
             response.status(HttpStatusCode.OK).send(user);
         } catch (error) {
-            response.status(HttpStatusCode.NOT_FOUND).send(error);
+            response.status(HttpStatusCode.NOT_FOUND).send(error instanceof Error ? error.message : "Erro na solicitação.");
         }
     }
 
     public deleteUserById = async (request: Request, response: Response) => {
         try {
-            const verifyUser = await this.userRepository.getUserById(request.params.id)
-            if (verifyUser == null) {
-                response.status(HttpStatusCode.BAD_REQUEST).send("Não foi possível encontrar o usuário.")
-                return;
-            } else if (verifyUser.services.length > 0) {
-                response.status(HttpStatusCode.BAD_REQUEST).send("Não pode excluir esse usuário pois ele possui serviços ativos")
-                return;
-            } else if (verifyUser.schedule.length > 0) {
-                response.status(HttpStatusCode.BAD_REQUEST).send("Não pode excluir esse usuário pois ele possui agendamentos ativos")
-                return;
-            }
-            const user = await this.userRepository.deleteUser(request.params.id);
+            const user = await this.userService.deleteUser(request.params.id)
             response.status(HttpStatusCode.OK).send("Usuário excluído com sucesso!");
         } catch (error) {
-            response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send("erro na solicitação.");
+            response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send(error instanceof Error ? error.message : "Erro na solicitação.");
         }
     }
 
--- reservation-backend/src/services/UserService.ts
+++ reservation-backend/src/services/UserService.ts
@@ -2,8 +2,10 @@ import HttpStatusCode from '../utils/enum/HttpStatusCode';
 import { UserRepository } from '../database/User.repository';
 import { generateToken } from '../utils/GenerateToken';
 import bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
+import { userValidation } from '../validations/User.validation';
 
-export default class UserServices {
+export class UserServices {
     public repository: UserRepository;
 
     constructor() {
@@ -26,7 +28,62 @@ export default class UserServices {
             };
         }
         const token = generateToken(user.id, user.role);
-        
+
         return { token };
+    }
+
+    public create = async (data: User) => {
+        await userValidation.validate(data);
+        const hashPassword = await bcrypt.hash(data.password, 10);
+        data.password = hashPassword;
+        const user = await this.repository.createUser(data);
+        return user;
+    }
+
+    public getUsers = async () => {
+        try {
+            const users = await this.repository.getUsers();
+            return users;
+        } catch (error) {
+            throw new Error("Solicitação não concluída");
+        }
+    }
+
+    public getUserById = async (id: string) => {
+            if (!id) {
+                throw new Error("O id não foi fornecido.");
+            }
+            const user = await this.repository.getUserById(id);
+            if (!user) {
+                throw new Error("Usuário não encontrado.");
+            }
+            return user;
+    }
+
+    public updateUser = async (id: string, data: User) => {
+            if (data.role === "CLIENT") {
+                const user = await this.repository.getUserById(id);
+                if (user && user.services.length > 0) {
+                    throw new Error("Usuário com serviços não pode ser definido como CLIENT.");
+                }
+            }
+            await userValidation.validate(data);
+            const hashPassword = await bcrypt.hash(data.password, 10);
+            data.password = hashPassword;
+            const user = await this.repository.updateUser(id, data);
+            return user;
+    }
+
+    public deleteUser = async (id: string) => {
+        const verifyUser = await this.repository.getUserById(id);
+        if (verifyUser == null) {
+            throw new Error("Não foi possível encontrar o usuário.");
+        } else if (verifyUser.services.length > 0) {
+            throw new Error("Não pode excluir esse usuário pois ele possui serviços ativos");
+        } else if (verifyUser.schedule.length > 0) {
+            throw new Error("Não pode excluir esse usuário pois ele possui agendamentos ativos");
+        }
+        const user = await this.repository.deleteUser(id);
+        return user;
     }
 }
