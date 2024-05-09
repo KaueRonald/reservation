7d2e776d6623bea806cd0088e62bc619add64f53
KaueRonald <Kaueronald21.kr@gmail.com>
2024-05-08 21:29:21 -0300

    feat: redoc try and redoc standalone with swagger

--- reservation-backend/src/index.ts
+++ reservation-backend/src/index.ts
@@ -2,7 +2,8 @@ import express from "express"
 import cors from "cors";
 import dotenv from "dotenv";
 import { UserRoutes } from "./routes/User.routes";
-
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 
 dotenv.config()
 
@@ -10,6 +11,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get('/swagger', (req, res) => {
+    res.sendFile(process.cwd() + '/src/swagger.json');
+})
+app.get('/docs', (req, res) => {
+    res.sendFile(process.cwd() + '/index.html');
+})
 app.use(UserRoutes)
 
 app.listen(3001, () => {
