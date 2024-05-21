eb64bca5e44987a018066ab71e620276ba413483
KaueRonald <Kaueronald21.kr@gmail.com>
2024-05-20 21:46:28 -0300

    feat: deleting navabr buttons and increasing sidebar

--- reservation-frontend/src/components/navbar/index.tsx
+++ reservation-frontend/src/components/navbar/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { AppBar, Box, Button, Toolbar } from "@mui/material"
+import { AppBar, Box, Button, Modal, Toolbar } from "@mui/material"
 import styles from "./index.module.scss"
 import Image from "next/image"
 import agendalogo from "../../../public/Agendalogo.png"
@@ -7,29 +7,10 @@ import Link from "next/link"
 import { getStorage, clearStorage } from "@/utils/storage"
 import { decodeToken } from "react-jwt"
 import { useEffect, useState } from 'react';
+import { Sidebar } from "../sidebar"
+import CloseIcon from '@mui/icons-material/Close';
 
 export const NavBar = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [role, setRole] = useState('PROVIDER');
-
-    useEffect(() => {
-        const token = getStorage('token');
-        setIsLoggedIn(!!token);
-
-        function tokenRole(token: string) {
-            if (token) {
-                const decoded = decodeToken(token);
-                return decoded as { role: string };
-            }
-            return null;
-        }
-
-        const decodedToken = token ? tokenRole(token) : null;
-        const userRole = decodedToken ? decodedToken.role : 'PROVIDER';
-
-        setRole(userRole);
-    }, []);
-
     return (
         <Box className={styles.container}>
             <AppBar className={styles.nav} position="static">
@@ -40,25 +21,7 @@ export const NavBar = () => {
                         </Link>
                         <p className={styles.logoname}>AgendaSJP</p>
                     </Box>
-                    <Box className={styles.btns}>
-                        {isLoggedIn ? (
-                            <Box className={styles.btnscontainer}>
-                                <Button href="/" className={styles.btn} variant="outlined">Perfil</Button>
-                                {role === 'PROVIDER' && (
-                                    <Button href="/serviços" className={styles.btn} variant="outlined">Meus Serviços</Button>
-                                )}
-                                <Button href="/agendamentos" className={styles.btn} variant="outlined">Meus Agendamentos</Button>
-                                <Button href="/" className={styles.btn} variant="outlined" onClick={clearStorage}>Sair</Button>
-                            </Box>
-                        ) : (
-                            <Box className={styles.btnscontainer}>
-                                <Button href="/login" className={styles.btn} variant="outlined">Login</Button>
-                                <Button href="/register" className={styles.btn} variant="outlined">Cadastrar</Button>
-                            </Box>
-                        )}
-                    </Box>
-
-                    
+                    <Sidebar />
                 </Toolbar>
             </AppBar>
         </Box>
